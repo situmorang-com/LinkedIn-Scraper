229c1f923d485f5fc7706efd0bfe82df6925e4da
Edmund Situmorang <edmundsitumorang@gmail.com>
2020-01-13 13:06:30 +0700

    added updated linkedin browser elements

---
+++ LinkedIn-Scraper.js
@@ -0,0 +1,5 @@
+let name = document.getElementsByClassName("inline t-24 t-black t-normal break-words")[0].innerText
+const experienceSection = document.getElementById("experience-section")
+const companyElement = experienceSection.querySelector(".pv-entity__company-summary-info")
+let companyName = companyElement.querySelector("h3 > span:nth-child(2)").textContent
+const titleElement = experienceSection.querySelector(".pv-entity__company-summary-info")
